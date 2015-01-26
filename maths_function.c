2efb60d3faff866fef30b4e375dde3cc8d7bbbec
Mihir Pathak <P1322361X@GH584-003.lec-admin.dmu.ac.uk>
2015-01-26 14:08:07 +0000

    added a comment

--- maths_function.c
+++ maths_function.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main(){
+	//This is the line causing issue in the conflict branch
 	
 	printf("Hello world \n");
 	printf("Fare thee well! \n")
 	printf("yo yo  \n");
+
+	printf(" hello from mihir \n");
 }
 
